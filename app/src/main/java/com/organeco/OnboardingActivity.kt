0221136a824700c5cff1d8ef7dce593a38c83dca
amarsh30 <rzhamr06@gmail.com>
2023-05-26 23:33:24 +0700

    update onBoarding

--- app/src/main/java/com/organeco/OnboardingActivity.kt
+++ app/src/main/java/com/organeco/OnboardingActivity.kt
@@ -7,5 +7,6 @@ class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        supportActionBar?.hide()
     }
 }
