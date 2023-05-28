4fc6be96cd30d0313d74ea5d89c25808cfb5ceb9
amarsh30 <rzhamr06@gmail.com>
2023-05-28 18:42:31 +0700

    update SplashScreen

--- app/src/main/java/com/organeco/SplashScreenActivity.kt
+++ app/src/main/java/com/organeco/SplashScreenActivity.kt
@@ -1,11 +1,38 @@
 package com.organeco
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.organeco.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private var binding: ActivitySplashScreenBinding? = null
+    private val time = 3000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        supportActionBar?.hide()
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(time)
+                    val intent = Intent(this@SplashScreenActivity, OnboardingActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finishAffinity()
     }
 }
